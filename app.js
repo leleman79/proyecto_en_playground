5925a83cebdd860e5cdab333390025ce1b3c826d
leleman79 <leleman79@gmail.com>
2021-09-20 19:19:20 -0300

    Creando primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+
+app.use(express.static('public'));
+
+app.listen(3030, () => {
+    console.log('Servidor corriendo en puerto 3030');
+});
