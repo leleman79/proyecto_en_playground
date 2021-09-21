aa4836ff1e58a78289e9941f07b75c6ce7ba3cc7
leleman79 <leleman79@gmail.com>
2021-09-20 22:43:11 -0300

    Arreglos varios

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname + '/views/home.html'))
 });
 
 
