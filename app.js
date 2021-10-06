a44a90a81bc17f66ba30153c5d460dcbf28b556d
leleman79 <leleman79@gmail.com>
2021-10-06 20:14:29 -0300

    Proyecto casi terminado

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const publicPath = path.resolve(__dirname, './public')
 
-app.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname + '/views/home.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
 });
 
 
